57329327dc07e29c80e257b6c9823bee3af5655c
ifirdavs <fismoilov20@gmail.com>
2023-12-18 01:19:56 +0500

    add: added api/admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Prices)
+admin.site.register(Address)
+admin.site.register(Users)
+admin.site.register(ContactInfo)
+admin.site.register(Licenses)
+admin.site.register(Agents)
+admin.site.register(Customers)
+admin.site.register(Reviews)
+admin.site.register(Estate)
+admin.site.register(Amenities)
+admin.site.register(Contracts)
+admin.site.register(Favorites)
+admin.site.register(Posts)
+
