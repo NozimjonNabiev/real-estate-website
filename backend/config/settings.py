ff2c0440788dff03d5450f8fa5d834a50e3e1f39
ifirdavs <fismoilov20@gmail.com>
2024-01-14 00:58:52 +0500

    Extracted DB data to data.json

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str, default='django-123')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["firdavsbek.pythonanywhere.com"]
 
 
 # Application definition
