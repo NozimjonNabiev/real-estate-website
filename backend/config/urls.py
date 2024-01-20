1f0eeab63959459a7f044938cfb13815f87d05f9
Bobosher <bobosher.work@gmail.com>
2024-01-20 16:33:13 +0500

    fix: Removed api/ prefix from urlpatterns since this part is gonna be handled by reverse proxy.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/v1/', include('api.urls'), name='api'),
+    path('v1/', include('api.urls'), name='api'),
     path('admin/', admin.site.urls),
 ]
 
