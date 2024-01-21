ce4f5cc86459a1974afbfc7a35fb8fdab66f488b
Bobosher <bobosher.work@gmail.com>
2024-01-22 00:51:14 +0500

    feat: Added regression model for market value prediction.

--- backend/api/regression/model.py
+++ backend/api/regression/model.py
@@ -1,11 +1,14 @@
 import pickle
+import os
+from pathlib import Path
 from .tf import Model
 
 
 class RegressionModel(Model): pass
 
 
-model = RegressionModel(pickle.load(open("real_estate_model_v2.pkl", "rb")))
+pickled_model_file_path = os.path.join(Path(__file__).resolve().parent, "real_estate_model_v2.pkl")
+model = RegressionModel(pickle.load(open(pickled_model_file_path, "rb")))
 
 
 def predict_market_value(district: str, bedroom_count: int, area: int) -> int:
